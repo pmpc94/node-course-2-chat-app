b09b55b50e4857ac5101a07887c4aaef063669ab
pmpc94 <pmig.pc@gmail.com>
2018-11-16 20:28:56 +0100

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+  var socket = io();
+
+  socket.on('connect', function () {
+    console.log('Connected to server')
+
+    socket.emit('createMessage' ,{
+      from: 'pedro@example.com',
+      text: 'First Socket.io'
+    });
+  });
+
+  socket.on('disconnect', function () {
+    console.log('Disconnected from server')
+  });
+
+  socket.on('newMessage', function (message) {
+    console.log('New message', message);
+  });
+
+  socket.on()
