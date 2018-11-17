9767dd27bad672c7d132a7c11011aaf31740504e
pmpc94 <pmig.pc@gmail.com>
2018-11-18 00:34:37 +0100

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@
     var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-      from: 'User',
       text: messageTextbox.val()
     }, function() {
         messageTextbox.val('');
